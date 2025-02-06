a799b0975ce5bf4be20e48073f7c6f47a7660d22
Vishal Mishra <147994104+Vishal-Mishra27@users.noreply.github.com>
2025-02-07 01:03:10 +0530

    Update tailwind.config.js

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,17 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+export default async function () {
+  const tailwindcssAnimate = (await import("tailwindcss-animate")).default;
+
+  return {
     darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-  theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
-  },
-  plugins: [tailwindcssAnimate()],
-};
+    theme: {
+      extend: {
+        borderRadius: {
+          lg: 'var(--radius)',
+          md: 'calc(var(--radius) - 2px)',
+          sm: 'calc(var(--radius) - 4px)'
+        },
+        colors: {}
+      }
+    },
+    plugins: [tailwindcssAnimate()],
+  };
+}
 
